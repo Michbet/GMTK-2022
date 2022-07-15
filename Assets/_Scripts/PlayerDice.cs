301dd89d5ac2ba83e085386da146fd3eb0d3e19a
Tryston Minsquero <tam190005@utdallas.edu>
2022-07-15 13:16:20 -0500

    basic Die script

---
+++ Assets/_Scripts/PlayerDice.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDice : MonoBehaviour
+{
+    public Die speedDie;
+    public Die blockDie;
+    public Die attackDie;
+}
