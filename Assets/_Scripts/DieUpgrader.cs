5c255f27ac48ef1dd0b0317555d7340f828474ab
Tryston Minsquero <tam190005@utdallas.edu>
2022-07-15 14:30:06 -0500

    added upgrade UI scripts and prefabs

---
+++ Assets/_Scripts/DieUpgrader.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class DieUpgrader : MonoBehaviour
+{
+    public DieUpgradeFace[] dieUpgradeSlots;
+
+    private Die _die;
+
+    private void Start()
+    {
+        _die = new Die
+        {
+            faces = new int[] {1, 2, 3, 4, 5, 6}
+        };
+        SetUpSlots(_die);
+    }
+
+    public void SetUpSlots(Die die)
+    {
+        _die = die;
+        for (int i = 0; i < _die.faces.Length; i++)
+        {
+            if(i < dieUpgradeSlots.Length)
+                dieUpgradeSlots[i].SetUp(_die, i);
+        }
+    }
+}
