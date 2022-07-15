ea5da3ff682125b09734f0da4778a1743e73870a
Tryston Minsquero <tam190005@utdallas.edu>
2022-07-15 13:39:48 -0500

    added TMP

---
+++ Assets/DieUpgradeSlot.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DieUpgradeSlot : MonoBehaviour
+{
+    private Die _die = null;
+    private int _index;
+
+    public TMP_Text text;
+
+    public void SetUp(Die die, int index)
+    {
+        this._die = die;
+        this._index = index;
+    }
+
+    public void Set(int value)
+    {
+        _die.faces[_index] = value;
+    }
+}
---
+++ Assets/DieUpgradeUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieUpgradeUI : MonoBehaviour
+{
+    public DieUpgradeSlot[] dieUpgradeSlots;
+
+    private Die die;
+
+    public void SetSlots()
+    {
+        
+    }
+}
