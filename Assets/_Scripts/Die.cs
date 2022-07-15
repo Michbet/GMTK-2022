d12d94d31beb012729f3ffdc9a27fda8f20acc1c
Tryston Minsquero <tam190005@utdallas.edu>
2022-07-15 13:12:25 -0500

    feat: add die scripts

---
+++ Assets/_Scripts/Die.cs
@@ -0,0 +1,20 @@
+using System;
+using Random = UnityEngine.Random;
+
+[System.Serializable]
+public class Die
+{
+    // faces for the dice, can be changed in the editor
+    public int[] faces = new int[] {1, 2, 3, 4, 5, 6};
+
+    public int Roll()
+    {
+        // throw an error if faces doesn't exist
+        if (faces == null || faces.Length <= 0)
+            throw new Exception("can't roll die with no faces");
+        
+        
+        var index = Random.Range(0, faces.Length);
+        return faces[index];
+    }
+}
