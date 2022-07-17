3bb4904a2184940ab9ea11fbb6739ddbcd335258
Tryston Minsquero <tam190005@utdallas.edu>
2022-07-17 07:41:30 -0500

    Update LeaderboardManager.cs

--- Assets/_Scripts/LeaderboardManager.cs
+++ Assets/_Scripts/LeaderboardManager.cs
@@ -1,9 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using LootLocker.Requests;
-using LootLocker.Admin;
 
 public class LeaderboardManager : MonoBehaviour
 {
