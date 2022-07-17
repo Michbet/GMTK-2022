7250e7881adbdf156fc213e60d7cf1fe7fa1063b
Tryston Minsquero <tam190005@utdallas.edu>
2022-07-16 23:57:45 -0500

    reset gold count

--- Assets/_Scripts/ShopManager.cs
+++ Assets/_Scripts/ShopManager.cs
@@ -29,6 +29,7 @@ public class ShopManager : MonoBehaviour
         speedDieUpgrader.SetUpSlots(player.dice.speedDie);
         blockDieUpgrader.SetUpSlots(player.dice.blockDie);
         attackDieUpgrader.SetUpSlots(player.dice.attackDie);
+        goldCount = 0;
     }
 
     public void SetUp(StatsDice nextEnemyDice)
